a392227c0ecae2441a93d2d3c693fae4f03ff33e
Arnar Kári <arnarkari93@gmail.com>
2023-12-05 14:50:14 +0000

    day3 part2

--- day3/day3.go
+++ day3/day3.go
@@ -10,6 +10,7 @@ func main() {
 	filePath := utils.GetFilePathFromArgs()
 	input := utils.ReadFileToArray(filePath)
 	part1(input)
+	part2(input)
 }
 
 type Point struct {
@@ -34,6 +35,14 @@ func getPointOnMap(point Point, engine map[Point]rune) rune {
 		return entry
 	}
 	return '.' // default out of bounds as '.'
+}
+
+func getNumberOnMap(point Point, engine map[Point]int) int {
+	entry, exists := engine[point]
+	if exists {
+		return entry
+	}
+	return -1 // no number here
 }
 
 func down(point Point) Point {
@@ -76,7 +85,6 @@ func checkIfAdjecentToSymbol(point Point, engine map[Point]rune) bool {
 	}
 	return false
 }
-
 
 func shouldAddNumberToSum(isAdjecentToSymbol bool, numberQue []rune, char rune, line string, position int) bool {
 	var isAtEndOfLine = len(line)-1 == position
@@ -122,4 +130,97 @@ func part1(input []string) {
 	}
 
 	fmt.Println("Part sum", sum)
+}
+
+func getEngineNumberCord(input []string) map[Point]int {
+  var engineNumber = make(map[Point]int)
+
+	for y, line := range input {
+    var numberQue = []rune{}
+    var points = []Point{}
+
+		for x, char := range line {
+      if unicode.IsDigit(char) {
+        points = append(points, Point{x, y})
+        numberQue = append(numberQue, char)
+      }
+
+      if len(numberQue) > 0 && (!unicode.IsDigit(char) || len(line)-1 == x) {
+        for _, point := range points {
+          engineNumber[point] = utils.DangerouslyParseInt(string(numberQue))
+        }
+        numberQue = []rune{}
+        points = []Point{}
+      }
+		}
+	}
+  
+  return engineNumber
+}
+
+func part2(input []string) {
+	fmt.Println("Part 2")
+	utils.PrintInput(input)
+
+  var pointToNumberMap = getEngineNumberCord(input)
+
+  var sum = 0
+  for y, line := range input {
+    for x, char := range line {
+      if char == '*' {
+        var gearPoint = Point{x, y}
+
+        var numbersFound = []int{}
+        // look up
+        if num, exists := pointToNumberMap[left(up(gearPoint))]; exists {
+          numbersFound = append(numbersFound, num)
+          
+          // check if there is a number to the right of found number
+          if _, isSameNumber := pointToNumberMap[up(gearPoint)]; !isSameNumber {
+            // there needs to be space between the two numbers
+            if num, exists := pointToNumberMap[right(up(gearPoint))]; exists {
+              numbersFound = append(numbersFound, num)
+            }
+          }
+
+
+        } else if num, exists := pointToNumberMap[up(gearPoint)]; exists {
+          numbersFound = append(numbersFound, num)
+        } else if num, exists := pointToNumberMap[right(up(gearPoint))]; exists {
+          numbersFound = append(numbersFound, num)
+        }
+        // look left
+        if num, exists := pointToNumberMap[left(gearPoint)]; exists {
+          numbersFound = append(numbersFound, num)
+        }
+        // look right
+        if num, exists := pointToNumberMap[right(gearPoint)]; exists {
+          numbersFound = append(numbersFound, num)
+        }
+        // look down
+        if num, exists := pointToNumberMap[left(down(gearPoint))]; exists {
+          numbersFound = append(numbersFound, num)
+
+          // check if there is a number to the right of found number
+          if _, isSameNumber := pointToNumberMap[down(gearPoint)]; !isSameNumber {
+            // there needs to be space between the two numbers
+            if num, exists := pointToNumberMap[right(down(gearPoint))]; exists {
+              numbersFound = append(numbersFound, num)
+            }
+          }
+        } else if num, exists := pointToNumberMap[down(gearPoint)]; exists {
+          numbersFound = append(numbersFound, num)
+        } else if num, exists := pointToNumberMap[right(down(gearPoint))]; exists {
+          numbersFound = append(numbersFound, num)
+        }
+
+        // found gear
+        if len(numbersFound) == 2 {
+          sum += numbersFound[0] * numbersFound[1]
+        }
+      }
+    }
+  }
+
+  fmt.Println("Part 2 sum", sum)
 }
