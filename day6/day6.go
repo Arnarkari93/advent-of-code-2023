9f788abd56ab9918c7df8073a5b7b39b2668dd16
Arnar Kári <arnarkari93@gmail.com>
2023-12-10 13:19:18 +0000

    day6 constant

--- day6/day6.go
+++ day6/day6.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"math"
 	"strings"
-	"time"
 )
 
 func calcDiscriminant(a, b, c float64) float64 {
@@ -32,18 +31,8 @@ func main() {
 	filePath := utils.GetFilePathFromArgs()
 	input := utils.ReadFileToArray(filePath, "\n")
 
-	Part1Constant(input)
 	Part1(input)
-
-	start2 := time.Now()
 	Part2(input)
-	elapsed2 := time.Since(start2)
-	fmt.Println("Part 2 took", elapsed2)
-
-	start22 := time.Now()
-	Part2Constant(input)
-	elapsed22 := time.Since(start22)
-	fmt.Println("Part 2 Const took", elapsed22)
 }
 
 func getDistance(time, record, min int) int {
@@ -53,30 +42,6 @@ func getDistance(time, record, min int) int {
 }
 
 func Part1(input []string) {
-	times := strings.Fields(input[0])[1:]
-	distances := strings.Fields(input[1])[1:]
-
-	totalPlays := []int{}
-	for i := 0; i < len(times); i++ {
-		plays := 0
-		time := utils.DangerouslyParseInt(times[i])
-		record := utils.DangerouslyParseInt(distances[i])
-		for j := 0; j < time; j++ {
-			var timeLeft = time - j
-			var distance = timeLeft * j
-			if distance > record {
-				plays += 1
-			}
-		}
-
-		fmt.Println(time, record, "plays", plays)
-		totalPlays = append(totalPlays, plays)
-	}
-
-	fmt.Println("Part 1: ", Product(totalPlays))
-}
-
-func Part1Constant(input []string) {
 	times := strings.Fields(input[0])[1:]
 	distances := strings.Fields(input[1])[1:]
 
@@ -101,31 +66,11 @@ func Part1Constant(input []string) {
 
 func Part2(input []string) {
 	t := strings.Join(strings.Fields(input[0])[1:], "")
-	distance := strings.Join(strings.Fields(input[1])[1:], "")
-
-	time := utils.DangerouslyParseInt(t)
-	record := utils.DangerouslyParseInt(distance)
-	plays := 0
-	for j := 0; j < time; j++ {
-		var timeLeft = time - j
-		var distance = timeLeft * j
-		if distance > record {
-			plays += 1
-		}
-	}
-
-	fmt.Println(time, record, "plays", plays)
-
-	fmt.Println("Part 2: ", plays)
-}
-
-func Part2Constant(input []string) {
-	t := strings.Join(strings.Fields(input[0])[1:], "")
 	r := strings.Join(strings.Fields(input[1])[1:], "")
 
 	time := utils.DangerouslyParseInt(t)
 	record := utils.DangerouslyParseInt(r)
-  min, max := quadraticFormula(1, float64(time), float64(record))
+	min, max := quadraticFormula(1, float64(time), float64(record))
 
 	minDistance := getDistance(time, record, int(math.Floor(min)))
 	if minDistance == record {
@@ -133,6 +78,6 @@ func Part2Constant(input []string) {
 	}
 	plays := math.Floor(max) - math.Floor(min)
 
-	fmt.Println(time, record, "plays", plays)
-	fmt.Println("Part 2: ", plays)
+	fmt.Println(time, record, "plays", int(plays))
+	fmt.Println("Part 2: ", int(plays))
 }
