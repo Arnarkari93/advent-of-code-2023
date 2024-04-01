fd2a3ac3ef06bf036d5c2273b87ffec3d976b0a8
Arnar Kári <arnarkari93@gmail.com>
2024-04-01 09:30:09 +0000

    day12

---
+++ day12/day12.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"advent_of_code_2023/utils"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	filePath := utils.GetFilePathFromArgs()
+	input := utils.ReadFileToArray(filePath, "\n")
+	input = input[:len(input)-1]
+
+	Part1(input)
+}
+
+func ParseLine(line string) (string, []int) {
+	fields := strings.Fields(line)
+	springs, groupsString := fields[0], fields[1]
+
+	var groups []int
+	for _, num := range strings.Split(groupsString, ",") {
+		groups = append(groups, utils.DangerouslyParseInt(num))
+	}
+
+	return springs, groups
+}
+
+func Part1(input []string) {
+	sum := 0
+	for _, line := range input {
+		springs, groups := ParseLine(line)
+		m := make(map[string]int)
+		arrangements := SpringsArrangements(springs, groups, m)
+		sum += arrangements
+	}
+
+	fmt.Println("Total", sum)
+}
+
+func SpringsArrangements(springs string, groups []int, memo map[string]int) int {
+	if len(groups) == 0 {
+		if !strings.Contains(springs, "#") {
+			return 1
+		}
+		return 0
+	}
+
+	// not enough springs to satisfy the groups
+	if len(springs) < (Sum(groups) + len(groups) - 1) {
+		return 0
+	}
+
+	key := springs + fmt.Sprintf("%v", groups)
+	if _, found := memo[key]; found {
+		return memo[key]
+	}
+
+	// if current spring is operational we skip to the next one
+	if springs[0:1] == "." {
+		return SpringsArrangements(springs[1:], groups, memo)
+	}
+
+	total := 0
+	currentGroup := groups[0]
+
+	if !strings.Contains(springs[0:currentGroup], ".") &&
+		(len(springs) == currentGroup || springs[currentGroup:currentGroup+1] != "#") {
+		next := min(len(springs), currentGroup+1)
+		total += SpringsArrangements(springs[next:], groups[1:], memo)
+	}
+
+	if springs[0:1] != "#" {
+		total += SpringsArrangements(springs[1:], groups, memo)
+	}
+
+	memo[key] = total
+	return total
+}
+
+func Sum(groups []int) int {
+	sum := 0
+	for _, num := range groups {
+		sum += num
+	}
+	return sum
+}
